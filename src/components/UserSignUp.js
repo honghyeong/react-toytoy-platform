dde6d63c0c0e6bc3dd720ac9ab749a3e98d6af7b
honghyeong <scarletgold0601@gmail.com>
2021-11-28 13:43:43 +0900

    feat: UserSignUp

--- src/components/UserSignUp.js
+++ src/components/UserSignUp.js
@@ -2,9 +2,16 @@ import React from "react";
 
 const UserSignUp = () => {
   return (
-    <div>
-      <p>UserSignup</p>
-    </div>
+    <>
+      <style>
+        @import
+        url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100&display=swap');
+      </style>
+
+      <section>
+        <div className></div>
+      </section>
+    </>
   );
 };
 
